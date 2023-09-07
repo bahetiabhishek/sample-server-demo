d52d93b14b3db7eabfb83464ab867bd28773d468
abhishek <baheti.abhishek@gmail.com>
2023-09-07 13:49:12 +0200

    added new files

---
+++ include/Calc.hxx
@@ -0,0 +1,13 @@
+#ifndef INCL_HANDLER_LIBPLATON_WORKFLOW_REGISTER_CALLS_H_
+#define INCL_HANDLER_LIBPLATON_WORKFLOW_REGISTER_CALLS_H_
+
+#include <iostream>
+
+class Calculator
+{
+    public:
+    int calculate(int x, int y);
+};
+
+
+#endif /* INCL_HANDLER_LIBPLATON_WORKFLOW_REGISTER_CALLS_H_ */
---
+++ src/Calc.cxx
@@ -0,0 +1,6 @@
+#include "Calc.hxx"
+
+int Calculator::calculate(int x, int y)
+{
+    return x + y;
+}
